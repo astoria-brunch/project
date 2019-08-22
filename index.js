252dd2d593ccd378aa1d2075cd90bab8d93e5efe
Wayne Hong <woo.young.hong@hotmail.com>
2019-08-22 12:25:38 -0400

    initalized with a repository + index.js, .gitignore and added start script

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const $PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res, next) => {
+  try {
+    res.send('response is working!');
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.listen($PORT, () => {
+  console.log('server is listening at ', $PORT);
+});
