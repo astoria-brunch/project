1b7d44ca0b524dfe9c62477f135f89b22d71d32d
Wayne Hong <woo.young.hong@hotmail.com>
2019-08-26 13:58:48 -0400

    minor fix before merge

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const APIKey =
+  'Bearer ' +
+  'FyzJFB01i42g9yLpWYqY9rxMjmHeRH3Nz_5jXamI1CvVAZDVx-MjcXwzHSdxBkcgfrXphznltrGah2jKuoNrennGHbva6pTdEItKZFJcbVso9JyY2f1tOUpZ7BYdXXYx';
 // basic route set up
 
 // temp brunch data route * need to move APIKey to env var so that heroku allows access
 app.get('/brunch', async (req, res, next) => {
   try {
-    const APIKey =
-      'Bearer ' +
-      'FyzJFB01i42g9yLpWYqY9rxMjmHeRH3Nz_5jXamI1CvVAZDVx-MjcXwzHSdxBkcgfrXphznltrGah2jKuoNrennGHbva6pTdEItKZFJcbVso9JyY2f1tOUpZ7BYdXXYx';
     const response = await axios.get(
       'https://api.yelp.com/v3/businesses/search?location=astoria&categories=breakfast_brunch',
       {
