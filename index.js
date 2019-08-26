6dfaff7cffecbdc7217cca05a8589fddce595960
John Oncher <johnoncher@gmail.com>
2019-08-26 13:40:32 -0400

    trying to pull code

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const $PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res, next) => {
+app.get("/", (req, res, next) => {
   try {
-    res.send('response is working!');
+    res.send("response is working!");
   } catch (error) {
     next(error);
   }
 });
 
 app.listen($PORT, () => {
-  console.log('server is listening at ', $PORT);
+  console.log("server is listening at ", $PORT);
 });
